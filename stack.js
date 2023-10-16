1e1e189da5390a1e0a33f2ea3aff9dcaecc6458c
Dipesh B C <bcdipeshwork@gmail.com>
2023-10-16 16:44:09 -0400

    Implement stack

--- stack.js
+++ stack.js
@@ -20,26 +20,48 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const currentNode = new Node(val);
+    if (this.first === null && this.last === null) {
+      this.first = currentNode;
+      this.last = currentNode;
+    } else {
+      currentNode.next = this.first;
+      this.first = currentNode;
+    }
+    ++this.size;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.isEmpty()) {
+      throw Error("Stack is empty.");
+    }
 
+    const firstNode = this.first;
+    const secondNode = this.first.next;
+    firstNode.next = null;
+    this.first = secondNode;
+    --this.size;
+
+    return firstNode.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
+    if (this.isEmpty()) {
+      throw Error("Stack is empty.");
+    }
 
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 ? true : false;
   }
 }
 
